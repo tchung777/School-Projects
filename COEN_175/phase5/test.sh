1c3bfb59b63ab599d8cbe532646ec0f539e7b2a9
VersaceSauce <chchung@tesla.com>
2018-03-04 20:16:53 -0800

    modified test.sh

--- COEN_175/phase5/test.sh
+++ COEN_175/phase5/test.sh
@@ -9,8 +9,7 @@ do
     echo "------------------------"
     echo "Testing Against: ${filename}"
     cp ${filename}.c test.c
-    cp ${filename}$b.c testlib.c
-    if [ "$?" = "0" ]; then
+    if [ "cp ${filename}$b.c testlib.c" != "" ]; then
       cp ${filename}.out test.out
       ./scc < test.c > myTest.s
       gcc -m32 -w myTest.s testlib.c
