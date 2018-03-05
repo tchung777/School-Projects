011043081420408418f9cbfc1a681a76f52df5b5
VersaceSauce <chchung@tesla.com>
2018-03-04 20:26:50 -0800

    Shell script pissing me off..

--- COEN_175/phase5/test.sh
+++ COEN_175/phase5/test.sh
@@ -11,8 +11,8 @@ do
     cp ${filename}.c test.c
     cp ${filename}$b.c testlib.c
     cp ${filename}.out test.out
-    ./scc < test.c > myTest.s testlib.c
-    gcc -m32 -w myTest.s
+    ./scc < test.c > myTest.s
+    gcc -m32 -w myTest.s testlib.c
     ./a.out > myTest.out
     diff test.out myTest.out
 
