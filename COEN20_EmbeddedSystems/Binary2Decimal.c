eb07806e11ada1568a371ec48fe8c970c23cb2b5
Thomas Chung <cchung@scu.edu>
2019-03-16 22:11:16 -0700

    COEN20 stuff committed.

---
+++ COEN20_EmbeddedSystems/Binary2Decimal.c
@@ -0,0 +1,40 @@
+int pow(int x, int y) {
+    int ans=1;
+    int i;
+    for(i = 0; i < y; i++)
+        ans*=x;
+    return ans;
+}
+
+signed int Bin2Signed(int binary[8]){
+    int i;
+    if(!binary[8]) {
+        int flip = 0;
+        for(i = 0; i < 8; i++) {
+            if(flip)
+                binary[i]^=1;
+            if(binary[i])
+                flip = 1;
+        }
+        int integer = 0;
+        for(i = 0;i < 8;i++)
+            integer+=(binary[i]*pow(2,i));
+        return -1*integer;
+    } else {
+        int integer = 0;
+        for(i = 0;i < 8;i++)
+            integer+=(binary[i]*pow(2,i));
+        return integer;
+    }
+}
+
+unsigned int Bin2Unsigned(int binary[8]) {
+    int integer = 0;
+    int i;
+    for(i = 0;i < 8;i++)
+        integer+=(binary[i]*pow(2,i));
+    return integer;
+}
+
+
+
---
+++ COEN20_EmbeddedSystems/Decimal2Binary.c
@@ -0,0 +1,26 @@
+void Dec2Bin(int decimal, int binary[8]) {
+    int neg = 0;
+    int i;
+    if(decimal < 0) {
+        decimal*=-1;
+        neg = 1;
+    }
+    int index = 0;
+    int quotient = decimal;
+    for(i=0;i<8;i++) {
+        int bit = quotient % 2;
+        binary[index] = bit;
+        quotient /= 2;
+        index++;
+    }
+    if(neg) {
+        int flip = 0;
+        for(i = 0; i < 8; i++) {
+            if(flip)
+                binary[i]^=1;
+            if(binary[i])
+                flip = 1;
+        }
+    }
+}
+
